732f88b13e4d004a4dbc1b4c7a40ebc1c83b65f1
neel <theofficialneel@gmail.com>
2018-07-31 19:58:14 +0530

    resolution added

--- index.py
+++ index.py
@@ -1,4 +1,7 @@
 import os
 
+import commands
+x = commands.getstatusoutput("xdpyinfo | grep dimensions | awk '{print $2}'")
+print(x[1])
 print("Hello")
-os.system("ffmpeg -f x11grab -s 1366x768 -r 30 -i :0.0 grab.avi")
+os.system("ffmpeg -f x11grab -s "+x[1]+" -r 30 -i :0.0+0,0 grab.avi")
